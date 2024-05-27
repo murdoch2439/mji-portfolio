36774e901087bbcac9b9429fe38c868f4d493db7
Mji <murdochkhallz@gmail.com>
2024-05-27 15:36:40 +0100

    Portfolio review

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import Experience from "./components/experience/Experience";
+import Services from "./components/services/Services";
+import Portfolio from "./components/portfolio/Portfolio";
+import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+import About from "./components/about/About";
+import FooterSm from "./components/footerSm/FooterSm";
+import { GlobalStore} from './store/Context';
+
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <GlobalStore>
+          <div className="App">
+            <Header />
+            <Nav />
+            <About />
+            <Experience />
+            <Services />
+            <Portfolio />
+            <Testimonials />
+            <Contact />
+            <Footer />
+            <FooterSm />
+          </div>
+        </GlobalStore>
+      </>
   );
 }
 
