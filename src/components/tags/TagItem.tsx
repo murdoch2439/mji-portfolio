de905cf91d4e07c9c6174e6ca7a709f06e429c2b
Mji <murdochkhallz@gmail.com>
2024-05-27 15:44:18 +0100

    Portfolio review

---
+++ src/components/tags/TagItem.tsx
@@ -0,0 +1,20 @@
+import React, {FunctionComponent} from "react"
+import "./tagItem.css"
+
+
+type props={
+    title:string
+}
+
+
+
+const TagItem : FunctionComponent<props> =({title})=>{
+
+    return(
+        <div className={"tag__item_container"}>
+            <p className={"tag_title"}>{title}</p>
+        </div>
+    )
+}
+
+export default TagItem
