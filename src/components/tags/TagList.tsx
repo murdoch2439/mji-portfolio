51ddeaaf481a948c21b9acfc0c8109c479f68db8
Mji <murdochkhallz@gmail.com>
2024-05-27 15:46:51 +0100

    Portfolio review

---
+++ src/components/tags/TagList.tsx
@@ -0,0 +1,27 @@
+import React, {FunctionComponent} from "react"
+// import {BiCheck} from "react-icons/bi";
+import {BsCheck2Circle} from "react-icons/bs";
+import TagItem from "./TagItem";
+import "./taglist.css"
+
+type props={
+    items: string[],
+}
+
+
+const TagList : FunctionComponent<props> =({items})=>{
+    return(
+        <div className={"tag__list"}>
+            {
+                items.map((item, index)=>{
+                    return(
+                            <TagItem key={index} title={item} />
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+
+export default TagList
