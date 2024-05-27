d1ec747ec522afdd1946dc060b0f2b06c8f19f2d
Mji <murdochkhallz@gmail.com>
2024-05-27 22:31:58 +0100

    Portfolio review

--- src/components/services/Services.tsx
+++ src/components/services/Services.tsx
@@ -12,9 +12,6 @@ const UI_UXDESIGN=[
     "Landing page"
 ]
 
-
-
-
 const WEB_DEVELOPMENT=[
     "Research & analysis",
     "Features definition",
@@ -43,17 +40,6 @@ const Services : FunctionComponent =()=>{
             <h5>What I Offer</h5>
             <h2>Services</h2>
             <div className={"container services__container"}>
-                {/*<article className={"service"}>*/}
-                {/*    <div className={"service__head"}>*/}
-                {/*        <h3>UI/UX Design</h3>*/}
-                {/*        <ul className={"service__list"}>*/}
-                {/*            <li>*/}
-                {/*                <BiCheck className={"service__list-icon"} />*/}
-                {/*                <p>Lorem, ipsum dolor sit amet consecteturelit.</p>*/}
-                {/*            </li>*/}
-                {/*        </ul>*/}
-                {/*    </div>*/}
-                {/*</article>*/}
 
                 <ServiceCard title={"UI/UX Design"} >
                     <ServiceCardListItem items={UI_UXDESIGN} onClick={onListClick} />
