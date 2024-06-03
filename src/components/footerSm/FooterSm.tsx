8b364ab39523aa1b2fd3b0768fb8d127df3ce01d
Mji <murdochkhallz@gmail.com>
2024-06-03 16:09:09 +0100

    Portfolio review

--- src/components/footerSm/FooterSm.tsx
+++ src/components/footerSm/FooterSm.tsx
@@ -2,7 +2,6 @@ import React, {FunctionComponent} from "react"
 import './footerSm.css'
 // import {FaFacebookF} from "react-icons/fa"
 // import {FiInstagram} from "react-icons/fi"
-// import {IoLogoTwitter} from "react-icons/io"
 
 
 const FooterSm : FunctionComponent =()=>{
