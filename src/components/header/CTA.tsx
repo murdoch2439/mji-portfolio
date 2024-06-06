077bf42f84849438915eaeab14da0246bbd4984e
Mji <murdochkhallz@gmail.com>
2024-06-06 20:29:44 +0100

    Portfolio review

--- src/components/header/CTA.tsx
+++ src/components/header/CTA.tsx
@@ -4,6 +4,7 @@ import {useGlobalStore} from "../../store/Context";
 const CTA :FunctionComponent =()=>{
     const {setActiveSection}= useGlobalStore()
 
+
     return(
         <div className={"cta"}>
             <a
