611d282a6aecf832ad140c4d490b6757cd7aa7ee
Mji <murdochkhallz@gmail.com>
2024-06-06 20:30:01 +0100

    Portfolio review

--- src/components/experience/ExperienceItem.tsx
+++ src/components/experience/ExperienceItem.tsx
@@ -13,11 +13,6 @@ type props={
 
 
 
-
-
-
-
-
 const ExperienceItem : FunctionComponent<props> =({icon, title, experienceLevel})=>{
     return(
         <article className={"experience__details"}>
