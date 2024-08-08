f5fc14acdd78c6ecf66dcf0f9c1ec1d5efe49706
Mji <murdochkhallz@gmail.com>
2024-08-08 23:43:09 +0100

    Portfolio review

--- src/components/learning/Learning.tsx
+++ src/components/learning/Learning.tsx
@@ -40,6 +40,9 @@ const useNumber =(initialValue: number)=>useState<number>(initialValue)
 
 
 
+
+
+
 const CustomButton: FunctionComponent<DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>>& {title?:string}  =({title, children,style,  ...rest})=>{
     return(
         <button {...rest} style={{...style, backgroundColor:"green"}}>
