1c0cfa03afbfe646c0e5294fd068f03e5b7f8bcd
Mji <murdochkhallz@gmail.com>
2024-08-10 00:00:11 +0100

    Portfolio review

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -31,6 +31,9 @@ import {useGlobalStore} from "../../store/Context";
 
 
 
+
+
+
 const Header : FunctionComponent =()=>{
     const { setActiveSection }= useGlobalStore()
     // const [isToggled, setIsToggled]= useState(true)
