e3a4574c482a4472c340b732cb99b23d4d5cf873
Mji <murdochkhallz@gmail.com>
2024-08-15 23:42:11 +0100

    Portfolio review

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -30,6 +30,9 @@ import {useGlobalStore} from "../../store/Context";
 
 
 
+
+
+
 const Footer : FunctionComponent =()=>{
     // const {activeSection, setActiveSection}= useGlobalStore()
     const currentYear =  new Date().getFullYear();
