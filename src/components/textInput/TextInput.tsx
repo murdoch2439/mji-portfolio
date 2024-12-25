ea5069166a25e26950639cf2c9a759213de88b74
murdoch <murdochkhallz@gmail.com>
2024-12-25 19:03:15 +0100

    Managing contact form with emailJs

--- src/components/textInput/TextInput.tsx
+++ src/components/textInput/TextInput.tsx
@@ -6,6 +6,7 @@ type Props={
     label:string,
     isTextArea?:boolean,
     maxLength:number,
+    name:string
 }
 
 
@@ -15,17 +16,14 @@ type Props={
 
 
 
-
-
-
-const TextInput : FunctionComponent<Props> =({type, label, isTextArea, maxLength})=>{
+const TextInput : FunctionComponent<Props> =({type, label, isTextArea, maxLength, name})=>{
     return(
         <div className={"form"}>
             {
                 isTextArea ? <textarea  name={"message"} rows={7} placeholder={"Your Message"} required /> :
                     <>
-                        <input type={type||"text"} id={"email"} maxLength={maxLength}  className={"form__input"} autoComplete={"off"} placeholder={" "} required/>
-                        <label htmlFor={"email"} className={"form__label"}>{label}</label>
+                        <input type={type||"text"} name={name} maxLength={maxLength} className={"form__input"} autoComplete={"off"} placeholder={" "} required/>
+                        <label htmlFor={name} className={"form__label"}>{label}</label>
                     </>
             }
           {/*<input type={type||"text"} id={"email"}  className={"form__input"} autoComplete={"off"} placeholder={" "}/>*/}
