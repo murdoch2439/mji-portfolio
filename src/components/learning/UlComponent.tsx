dcbd568a9324c2fd62db6790a10c0618d37f2213
murdoch <murdochkhallz@gmail.com>
2024-12-26 12:07:54 +0100

    Managing contact form with emailJs

--- src/components/learning/UlComponent.tsx
+++ src/components/learning/UlComponent.tsx
@@ -1,32 +1,6 @@
 import React, {DetailedHTMLProps, HTMLAttributes, PropsWithChildren, ReactNode} from "react"
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 export function UL<T> ({items, render, itemClick, children}: PropsWithChildren<DetailedHTMLProps<HTMLAttributes<HTMLUListElement>, HTMLUListElement> & {items:T[], render:(item:T) => ReactNode, itemClick:(item:T)=> void}>){
     return(
         <>
