3b474ce147c348b5f309a7917a60424727c0280f
murdoch <murdochkhallz@gmail.com>
2025-02-08 06:07:44 +0100

    Managing automatic scroll as section gets updated

--- src/components/experience/ExperienceItem.tsx
+++ src/components/experience/ExperienceItem.tsx
@@ -7,51 +7,6 @@ type props={
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const ExperienceItem : FunctionComponent<props> =({icon, title, experienceLevel})=>{
     return(
         <article className={"experience__details"}>
