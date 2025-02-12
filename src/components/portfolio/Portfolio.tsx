15cc718b81b233e57220a16fd2f0e6b33f89f4e1
murdoch <murdochkhallz@gmail.com>
2025-02-12 04:27:59 +0100

    Managing automatic scroll as section gets updated

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -34,8 +34,6 @@ const Portfolio : FunctionComponent =()=>{
             <h5>My Recent Work</h5>
             <h2>Portfolio</h2>
             <div className={"container portfolio__container"}>
-               {/*<article className={"portfolio__item"}>*/}
-               {/*   <div className={"portfolio__item-image"}>*/}
 
                {/*    <h3>This is a portfolio item title</h3>*/}
                {/*    <a href={"https://github.com"} className={"btn"} target={"_blakc"}>Github</a>*/}
