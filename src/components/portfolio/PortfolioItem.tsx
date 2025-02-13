540c7dcd2ea55f8b7c9f5b7a2cf833d1cc19c047
murdoch <murdochkhallz@gmail.com>
2025-02-14 00:54:20 +0100

    Nav review

--- src/components/portfolio/PortfolioItem.tsx
+++ src/components/portfolio/PortfolioItem.tsx
@@ -17,7 +17,6 @@ const PortfolioItem : FunctionComponent<props> =({item})=>{
             <TagList  items={work}/>
             <div className={"portfolio__item-cta"}>
                 <a href={demo} className={"btn"} target={"__blank"}>See project</a>
-                {/*<a href={demo} className={"btn btn-primary"} target={"_blank"}>Visit the website</a>*/}
             </div>
         </article>
     )
